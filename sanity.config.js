b23584f47cec3308cb296b822f4b955af8bb7654
asseemm <assem.shimbergenova@jihc.kz>
2024-05-13 20:34:24 +0500

    install sanity

--- sanity.config.js
+++ sanity.config.js
@@ -6,16 +6,18 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
+import post from './sanity/schema/post'
+
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schema'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId: 'd8k6gjyj',
+  dataset: 'production',
   // Add and edit the content schema in the './sanity/schema' folder
-  schema,
+  schema: { types: [post] },
   plugins: [
     structureTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
