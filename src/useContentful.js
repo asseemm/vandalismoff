721ce8a390717d79fa9a7893c5a19e23922971ad
asseemm <assem.shimbergenova@jihc.kz>
2024-05-29 11:56:33 +0500

    form and about us

--- src/useContentful.js
+++ src/useContentful.js
@@ -3,7 +3,7 @@ import { createClient } from "contentful";
 const useContentful = () => {
   const client = createClient({
     space: "pkaakjur9k42",
-    accessToken: "9Qukq37zHQMy6RCvhjKbPPQnZndOyc3Cfif7LPSheFk",
+    accessToken: "9Qukq37zHQMy6RCvhjKbPPQnZndOyc3Cfif7LPSheFk   ",
     host: "preview.contentful.com"
   });
 
@@ -11,21 +11,12 @@ const useContentful = () => {
     try {
       const entries = await client.getEntries({
         content_type: "cases",
-        select: "fields.caseTitle,fields.caseSubtitle,fields.caseText"
+        select: "fields"
       });
-
-      const cases = entries.items.map((item) => {
-        return {
-          title: item.fields.caseTitle,
-          subtitle: item.fields.caseSubtitle,
-          text: item.fields.caseText.content[0].content[0].value
-        };
-      });
-
-      return cases;
+      
+      return entries
     } catch (error) {
-      console.error(`Error fetching cases: ${error}`);
-      return [];
+      console.log(`Error fetching authors ${error}`);
     }
   };
 
