53c3edae017d26b2bd7795ac36aed360d6dfd4e3
akenaaa <aruzhan.tolegen@jihc.kz>
2024-06-02 01:18:49 +0500

    team card

--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -3,98 +3,186 @@ import styled from 'styled-components';
 import DefaultLayout from "@/components/templates/defaultLayout";
 import BlockTitle from "@/components/atoms/BlockTitle";
 import Header from "@/components/organisms/Header";
+import Image from 'next/image';
+
+
 
 import Van3 from '@/assets/img/van3.jpeg'
+import { BlackTelegramSVG } from "@/assets/icon/BlackTelegramSVG";
+import { BlackInstagramSVG } from "@/assets/icon/BlackInstagramSVG";
+import { BlackMailSVG } from "@/assets/icon/BlackMailSVG";
+
 
 const Content = styled.section`
   max-width: 100%;
   height: auto; 
-  padding: 80px 10%;
-  padding-top: 20vh;
   padding: 100px 10%;
-
   @media screen and (max-width: 650px) {
       padding: 40px 10%;
   }
 `;
 
 const TeamContainer = styled.div`
-  padding: 50px 10%;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around left;
+  justify-content: space-between;
   align-items: flex-start;
+  height: 50vh;
+  margin-top: 5%;
 `;
 
 const TeamCard = styled.div`
-  background-color: #ffffff;
-//   box-shadow:0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px #8c5c93;   
-  width: 300px;
-  margin: 20px;
-  border-radius: 25px;
-  text-align: center;
-  transition: transform 0.3s;
-
-  &:hover {
-    transform: translateY(-5px);
-  }
+//    display: grid;
+  position: relative;
+  width: 22%;
+  height: 100%;
+  background-color: #ccc;
 `;
 
-const CardImage = styled.img`
+const TeamImg = styled.div`
+  width: 100%; 
+  height: 100%;
+`
+
+const Img = styled(Image)`
   width: 100%;
-  height: 200px;
+  height: 100%;
   object-fit: cover;
 `;
 
+
+const CardInfo = styled.div`
+  position: absolute;
+  bottom: -50px;
+  background-color: #FFF0D9;
+  width: 70%;
+  margin: 0px 10%;
+  padding: 5%;
+  text-align: center;
+  border-radius: 3px;
+  // box-shadow: 1px 1px 3px 1px #e2d7e7`;
+
 const CardTitle = styled.h3`
-font-family : "Jost", sans-serif;
-  margin: 20px 0;
+  font-family: "Jost", sans-serif;
+  margin: 10px 0;
   color: #333;
 `;
 
 const CardRole = styled.p`
-  margin-bottom: 20px;
   color: #666;
-  font-family : "Jost", sans-serif;
-  margin-bottom: 20%;
+  font-family: "Jost", sans-serif;
+  margin-bottom: 15px;
+`;
+const SocialIcons = styled.div`
+  display: flex; 
+  justify-content: center;
+  gap: 10px;
+  padding-bottom: 15px;
+  margin-top: 1%;
+`;
+
+const SocialIcon = styled.div`
+  width: 26px;  
+  height: 26px; 
 `;
 
 
 const TeamPage = () => {
-  const teamMembers = [
-    { name: "Джеффри Браун", role: "Главный тренер и директор", image: Van3 },
-    { name: "Крис Ричмонд", role: "Тренер и генеральный менеджер", image: Van3 },
-    { name: "Крис Ричмонд", role: "Тренер и генеральный менеджер", image: Van3 },
-    { name: "Крис Ричмонд", role: "Тренер и генеральный менеджер", image: Van3 },
-    { name: "Крис Ричмонд", role: "Тренер и генеральный менеджер", image: Van3 },
-    { name: "Дженни Робертс", role: "Тренер", image: Van3 },
-  ];
-
   return (
     <TeamContainer>
-      {teamMembers.map(member => (
-        <TeamCard key={member.name}>
-          <CardImage src={member.image} alt={member.name} />
-          <CardTitle>{member.name}</CardTitle>
-          <CardRole>{member.role}</CardRole>
-        </TeamCard>
-      ))}
+      <TeamCard key="Наг Рейнольдс">
+        <TeamImg>
+           <Img src={Van3} alt='img' />
+        </TeamImg>
+        <CardInfo>
+            <CardTitle>Наг Рейнольдс</CardTitle>
+            <CardRole>Финансовый директор</CardRole>
+            <SocialIcons>
+              <SocialIcon>
+              <BlackTelegramSVG/>
+              </SocialIcon>
+              <SocialIcon>
+              <BlackInstagramSVG/>
+              </SocialIcon>
+              <SocialIcon>
+              <BlackMailSVG/>
+              </SocialIcon>
+            </SocialIcons>
+        </CardInfo>
+      </TeamCard>
+      <TeamCard key="Наг Рейнольдс">
+        <TeamImg>
+           <Img src={Van3} alt='img' />
+        </TeamImg>
+        <CardInfo>
+            <CardTitle>Наг Рейнольдс</CardTitle>
+            <CardRole>Финансовый директор</CardRole>
+            <SocialIcons>
+              <SocialIcon>
+              <BlackTelegramSVG/>
+              </SocialIcon>
+              <SocialIcon>
+               <BlackInstagramSVG/>
+              </SocialIcon>
+              <SocialIcon>
+               <BlackMailSVG/>
+              </SocialIcon>
+            </SocialIcons>
+        </CardInfo>
+      </TeamCard>
+      <TeamCard key="Наг Рейнольдс">
+        <TeamImg>
+           <Img src={Van3} alt='img' />
+        </TeamImg>
+        <CardInfo>
+            <CardTitle>Наг Рейнольдс</CardTitle>
+            <CardRole>Финансовый директор</CardRole>
+            <SocialIcons>
+              <SocialIcon>
+              <BlackTelegramSVG/>
+              </SocialIcon>
+              <SocialIcon>
+              <BlackInstagramSVG/>
+              </SocialIcon>
+              <SocialIcon>
+              <BlackMailSVG/>
+              </SocialIcon>
+            </SocialIcons>
+        </CardInfo>
+      </TeamCard>
+      <TeamCard key="Наг Рейнольдс">
+        <TeamImg>
+           <Img src={Van3} alt='img' />
+        </TeamImg>
+        <CardInfo>
+            <CardTitle>Наг Рейнольдс</CardTitle>
+            <CardRole>Финансовый директор</CardRole>
+            <SocialIcons>
+              <SocialIcon>
+              <BlackTelegramSVG/>
+              </SocialIcon>
+              <SocialIcon>
+              <BlackInstagramSVG/>
+              </SocialIcon>
+              <SocialIcon>
+              <BlackMailSVG/>
+              </SocialIcon>
+            </SocialIcons>
+        </CardInfo>
+      </TeamCard>
+      {/* Добавьте других членов команды аналогичным образом */}
     </TeamContainer>
   );
-}
+};
 
 export default function Team() {
   return (
     <DefaultLayout>
-<<<<<<< HEAD
-        <Header backgroundcolor={'#804988'} color={'#fff'} displaymenuwhite={'block'} displayMenuPurple={'none'} />
-=======
-        <Header $backgroundcolor={'#804988'} color={'#fff'} $displaymenuwhite={'block'} displayMenuPurple={'none'}/>
->>>>>>> fc0a3b63bca648a962cf62f23406140579e08c6a
-        <Content>
-            <BlockTitle title="Наша команда" description="Все активисты команды ВандализмOFF" />
-            <TeamPage />
-        </Content>
+      <Header $backgroundcolor={'#804988'} color={'#fff'} $displaymenuwhite={'block'} $displayMenuPurple={'none'}/>
+      <Content>
+        <BlockTitle title="Наша  команда" description="Борцы против вандализма"/>
+        <TeamPage />
+      </Content>
     </DefaultLayout>
   );
 }
